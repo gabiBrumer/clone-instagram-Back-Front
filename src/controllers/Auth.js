13e7c8f7b9b4464942cb9452505dfb1da438a021
Gabrielle Brumer <gabriellebrumer@hotmail.com>
2022-07-21 09:30:09 -0300

    Iniciado o CRUD do instagram, com parte de cadastro e login finalizado

---
+++ src/controllers/Auth.js
@@ -0,0 +1,39 @@
+const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+
+const authController = {
+  showLogin: (req, res) => {
+    return res.render("auth/login");
+  },
+  showRegister: (req, res) => {
+    return res.render("auth/register");
+  },
+  store: async (req, res) => {
+    try {
+      const { email, name, username, password} = req.body;
+
+    const verifyUser = await User.findOne({where: {email}});
+
+    if(verifyUser) {
+      return res.render("auth/register", {error: 'Não foi possível realizar o cadastro!'});
+    }
+
+    const hash = hashSync(password, 10);
+
+    const user = await User.create({
+      email,
+      name,
+      username,
+      password: hash
+    })
+    console.log(user);
+    return res.redirect('/home');
+
+    } catch(error) {
+      console.log(error)
+      return res.render('auth/register', {error: "Sistema indisponível no momento!"})
+    }
+  }
+};
+
+module.exports = authController;
